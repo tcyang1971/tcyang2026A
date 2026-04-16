e5574f3c50199d7dcf4523e266b9a1312b226720
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-04-16 11:38:46 +0800

    爬取課程

--- web.py
+++ web.py
@@ -35,7 +35,22 @@ def index():
     link += "<a href=/account>POST傳值</a><hr>"
     link += "<a href=/read>讀取Firestore資料</a><hr>"
     link += "<a href=/read2>讀取Firestore資料(根據姓名關鍵字:楊)</a><hr>"
+    link += "<a href=/spider1>爬取子青老師本學期課程</a><hr>"
     return link
+
+@app.route("/spider1")
+def spider1():
+    R = ""
+    url = "https://www1.pu.edu.tw/~tcyang/course.html"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".team-box a")
+
+    for i in result:
+        R += i.text + i.get("href") + "<br>" 
+    return R
+
 
 @app.route("/read2")
 def read2():
