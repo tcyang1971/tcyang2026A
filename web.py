4cb320cd2551ce0df01a4e9e84b3d664ec8bd9ca
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-04-28 08:39:02 +0800

    2026-04-28更正

--- web.py
+++ web.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask, render_template, request
-from datetime import datetime
-
 import os
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
 
 # 判斷是在 Vercel 還是本地
 if os.path.exists('serviceAccountKey.json'):
@@ -22,39 +20,19 @@ else:
 firebase_admin.initialize_app(cred)
 
 
+from flask import Flask, render_template, request
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    link = "<h1>歡迎進入楊子青的網站20260409</h1>"
-    link += "<a href=/mis>課程</a><hr>"
-    link += "<a href=/today>現在日期時間</a><hr>"
-    link += "<a href=/me>關於我</a><hr>"
-    link += "<a href=/welcome?u=子青&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
-    link += "<a href=/account>POST傳值</a><hr>"
-    link += "<a href=/read>讀取Firestore資料</a><hr>"
-    link += "<a href=/read2>讀取Firestore資料(根據姓名關鍵字:楊)</a><hr>"
-    link += "<a href=/spider1>爬取子青老師課程資料</a><hr>"
-    link += "<a href=/movie1>爬取即將上映電影</a><hr>"
-    return link
+    return render_template("index.html")
 
-
-@app.route("/movie1")
-def movie1():
-    R = ""
-    url = "https://www.atmovies.com.tw/movie/next/"
-    Data = requests.get(url)
-    Data.encoding = "utf-8"
-    #print(Data.text)
-    sp = BeautifulSoup(Data.text, "html.parser")
-    result=sp.select(".filmListAllX li")
-    for item in result:
-        introduce = "https://www.atmovies.com.tw" + item.find("a").get("href")
-        R +=  "<a href=" + introduce + ">" + item.find("img").get("alt") + "</a><br>"
-        post = "https://www.atmovies.com.tw" + item.find("img").get("src")
-        R += "<img src=" + post + "> </img><br><br>" 
-    return R    
+@app.route("/test")
+def test():
+    return "呂恩妮"
 
 @app.route("/spider1")
 def spider1():
@@ -69,53 +47,62 @@ def spider1():
         R += i.text + i.get("href") + "<br>" 
     return R
 
-@app.route("/read2")
-def read2():
-    Result = ""
-    keyword = "楊"
+@app.route("/search", methods=["GET", "POST"])
+def search():
     db = firestore.client()
-    collection_ref = db.collection("靜宜資管2026B")    
-    docs = collection_ref.get()
-    for doc in docs: 
-        teacher = doc.to_dict()
-        if keyword in teacher["name"]:        
-            Result += str(teacher) + "<br>"
+    results = []
+    keyword = ""
+    
+    if request.method == "POST":
+        keyword = request.form.get("keyword")
+        collection_ref = db.collection("靜宜資管2026B")
+        docs = collection_ref.get()
 
-    if Result == "":
-        Result = "抱歉,查無此關鍵字姓名之老師資料"    
-    return Result
+        for doc in docs:
+            user = doc.to_dict()
+            if keyword in user["name"]:
+                results.append({
+                    "name": user["name"],
+                    "lab": user["lab"]
+                })
+
+    return render_template("search.html", results=results, keyword=keyword)
 
 @app.route("/read")
 def read():
-    Result = ""
     db = firestore.client()
-    collection_ref = db.collection("靜宜資管2026B")    
-    docs = collection_ref.get()
-    docs = collection_ref.order_by("lab", direction=firestore.Query.DESCENDING).get()
-    for doc in docs:         
-        Result += str(doc.to_dict()) + "<br>"    
-    return Result
+
+    Temp = ""
+    collection_ref = db.collection("靜宜資管2026B")
+    docs = collection_ref.order_by("lab", direction=firestore.Query.DESCENDING).limit(4).get()
+    for doc in docs:
+        Temp += str(doc.to_dict()) + "<br>"
+
+    return Temp
+
 
 @app.route("/mis")
 def course():
-    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
+    return "<h1>資訊管理導論</h1><a href=/>回到網站首頁</a>"
 
 @app.route("/today")
 def today():
     now = datetime.now()
-    return render_template("today.html", datetime=str(now))
+    year  = str(now.year)   # 取得年份 
+    month = str(now.month)  # 取得月份 
+    day   = str(now.day)    # 取得日期 
+    now = year + "年" + month + "月" + day + "日"
+    return render_template("today.html", datetime = now)
 
-@app.route("/me")
-def me():
-    return render_template("mis2026b.html")
+@app.route("/about")
+def about():
+    return render_template("mis2a.html")
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
-    user = request.values.get("u")
-    d = request.values.get("d")
-    c = request.values.get("c")    
-    return render_template("welcome.html", name= user, dep = d, course = c)
-
+    x = request.values.get("u")
+    y = request.values.get("dep")
+    return render_template("welcome.html", name = x, dep = y)
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
@@ -127,6 +114,84 @@ def account():
     else:
         return render_template("account.html")
 
+@app.route("/math", methods=["GET", "POST"])
+def math():
+    if request.method == "POST":
+        x = int(request.form["x"])
+        opt = request.form["opt"]
+        y = int(request.form["y"])      
+        result = "您輸入的是：" + str(x) + opt + str(y)
+        
+        if (opt == "/" and y == 0):
+            result += "，除數不能為0"
+        else:
+            match opt:
+                case "+":
+                    r = x + y
+                case "-":
+                    r = x - y
+                case "*":
+                    r = x * y
+                case "/":
+                    r = x / y  # 修正：之前誤寫為 x - y
+                case _:
+                    return "未知運算符號"
+            result += "=" + str(r)  + "<br><a href=/>返回首頁</a>"          
+        return result
+    else:
+        return render_template("math.html")
+
+@app.route('/cup', methods=["GET"])
+def cup():
+    # 檢查網址是否有 ?action=toss
+    #action = request.args.get('action')
+    action = request.values.get("action")
+    result = None
+    
+    if action == 'toss':
+        # 0 代表陽面，1 代表陰面
+        x1 = random.randint(0, 1)
+        x2 = random.randint(0, 1)
+        
+        # 判斷結果文字
+        if x1 != x2:
+            msg = "聖筊：表示神明允許、同意，或行事會順利。"
+        elif x1 == 0:
+            msg = "笑筊：表示神明一笑、不解，或者考慮中，行事狀況不明。"
+        else:
+            msg = "陰筊：表示神明否定、憤怒，或者不宜行事。"
+            
+        result = {
+            "cup1": "/static/" + str(x1) + ".jpg",
+            "cup2": "/static/" + str(x2) + ".jpg",
+            "message": msg
+        }
+        
+    return render_template('cup.html', result=result)
+
+
+
+@app.route("/math2", methods=["GET", "POST"])
+def math2():
+    result = None
+    if request.method == "POST":
+        # 取得使用者輸入
+        x = int(request.form.get("x"))
+        opt = request.form.get("opt")
+        y = int(request.form.get("y"))
+
+        # 你的核心邏輯
+        match opt:
+            case "∧":
+                result = x ** y
+            case "√":
+                if y != 0:
+                    result = x ** (1/y)
+                else:
+                    result = "數學上不存在「0 次方根」"
+            case _:
+                result = "請輸入∧(次方)或√(根號)"
+    return render_template("math2.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
