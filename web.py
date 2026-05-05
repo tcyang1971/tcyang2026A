1663f74ae0b2b28efeac065a093622eeb2cbee54
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-05-05 10:40:48 +0800

    road

--- web.py
+++ web.py
@@ -30,6 +30,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/road")
+def road():
+    R = ""
+    url = "https://newdatacenter.taichung.gov.tw/api/v1/no-auth/resource.download?rid=a1b899c0-511f-4e3d-b22b-814982a97e41"
+    Data = requests.get(url)
+    #print(Data.text)
+
+    JsonData = json.loads(Data.text)
+    for item in JsonData:
+        R += item["路口名稱"] + ",總共發生" + item["總件數"] + "件事故<br>"
+    return R
+
 @app.route("/movie3", methods=["GET", "POST"])
 def movie3():
     db = firestore.client()
