214d30bac3c2ebad6a576962413688b273fabb6f
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-05-12 10:59:10 +0800

    webhook1

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
 import random
 
@@ -29,6 +29,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+
+
 
 @app.route("/rate")
 def rate():
