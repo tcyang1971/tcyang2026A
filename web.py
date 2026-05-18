7d15e0cbd9d1aea4ce1a6262c45c7b17b618a7dc
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-05-19 07:38:47 +0800

    skip line

--- web.py
+++ web.py
@@ -41,7 +41,7 @@ def webhook():
     #info = "我是楊子青設計的電影聊天機器人, 動作：" + action + "； 查詢內容：" + msg
     if (action == "rateChoice"):
         rate =  req["queryResult"]["parameters"]["rate"]
-        info = "我是楊子青設計的電影聊天機器人, 您選擇的電影分級是：" + rate
+        info = "我是楊子青設計的電影聊天機器人,\n您選擇的電影分級是：" + rate
 
     return make_response(jsonify({"fulfillmentText": info}))
 
