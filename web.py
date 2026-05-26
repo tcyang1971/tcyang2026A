0c00eb8a02541e750ceabe64de619a34e744b60a
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-05-26 14:05:03 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -117,13 +117,13 @@ def webhook():
 
         # 2. 建立設定物件，設定你希望限制的最大 Token 數（例如 500）
         ai_config = types.GenerateContentConfig(
-            max_output_tokens = 300, # 壓低字數，換取 5 秒內安全通關
-            temperature = 0.3
+            max_output_tokens = 500, # 壓低字數，換取 5 秒內安全通關
+            temperature = 0.4
         )
 
 
         # 每次使用者拜訪該路徑時，直接使用全域的 client 呼叫模型
-        response = client.generate_content(
+        response = client.models.generate_content(
             model='gemini-3.5-flash',
             contents=req["queryResult"]["queryText"],
             config=ai_config,
