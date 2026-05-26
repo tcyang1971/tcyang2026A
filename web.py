65e5680243412009b1250c2f3c0edb76987b93d4
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-05-26 22:15:32 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -116,8 +116,8 @@ def webhook():
         #info =  req["queryResult"]["queryText"]
 
         ai_config = config=types.GenerateContentConfig(
-            max_output_tokens=150, 
-            system_instruction="你是一個簡短精確的助理，回答請控制在 50 字以內，直接講重點。"
+            max_output_tokens=500, 
+            system_instruction="你是一個簡短精確的助理，回答請控制在 100 字以內，直接講重點。"
         )
 
         response_stream = client.models.generate_content_stream(
