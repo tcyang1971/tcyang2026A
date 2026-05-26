89294f1fac576366fbd5ded4d543e427e442e1e4
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-05-26 11:24:00 +0800

    ai01

--- web.py
+++ web.py
@@ -106,6 +106,10 @@ def webhook():
 
         info += result
 
+    elif (action == "input.unknown"):
+        info =  req["queryResult"]["queryText"]
+
+
     return make_response(jsonify({"fulfillmentText": info}))
 
 
